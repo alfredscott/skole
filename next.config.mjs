23ea647711c3ff9e586d8a245438b400d38c0869
alfredscott <82605576+alfredscott@users.noreply.github.com>
2024-07-14 15:44:20 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/skole",
+    basePath: "skole/",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
